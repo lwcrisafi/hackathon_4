1f461e51df50b28da6fc3b48e27c63c87e7f2f65
mariecotije <kkmarushka@gmail.com>
2023-11-03 14:38:29 +0100

    fixed routes

--- src/RoutesSwitch.jsx
+++ src/RoutesSwitch.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import SubpageLayout from "./SubpageLayout";
-import Mockpage from "./Mockpage";
 import AuthorDetail from "./AuthorDetails";
 
 export default function RoutesSwitch({ AUTHOR_USERNAME }) {
@@ -16,7 +15,6 @@ export default function RoutesSwitch({ AUTHOR_USERNAME }) {
               path="/author/:AUTHOR_USERNAME"
               element={<AuthorDetail AUTHOR_USERNAME={AUTHOR_USERNAME} />}
             />
-            <Route path="/mockpage" element={<Mockpage />} />
             <Route path="*" element="404: page not found &#128561;" />
           </Route>
         </Routes>
