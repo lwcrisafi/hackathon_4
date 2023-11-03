371a031f43d638a2d35c631287b68db10d5e9409
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-03 13:04:30 +0100

    search query updated with small photos and access key as a variable

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,19 +1,25 @@
 import {useState, useEffect} from "react"
+import keys from './keys';
 
 export default function SearchResults ({searchQuery}){
     const [searchResults, setSearchResults]= useState([]);
+    const apiKey= keys.YOUR_ACCESS_KEY;
 
     useEffect(() => {
-         {
+        if (searchQuery) {
             
             const loadSearch = async () => {
-                
-                    const response = await fetch (`https://api.unsplash.com/search/photos?query=" + ${searchQuery} + "&client_id=okGiran4jPm-EhgDiqAO1Hz0gXs05AZGOFAhNq3N3jg`
+                try{
+                    
+                    const response = await fetch (`https://api.unsplash.com/search/photos?query=" + ${searchQuery} + &client_id=${apiKey}`
 
                     );
                     const data = await response.json();
                     console.log(data);
                     setSearchResults(data.results);
+                }catch (error){
+                    console.error ("Error fetching data:", error);
+                }
                 
             };
             loadSearch();
@@ -26,7 +32,7 @@ export default function SearchResults ({searchQuery}){
             {searchResults.map((results) => (
                 <div key={results.id}>
                     <h3>{results.alt_description}</h3>
-                    <img src={results.urls.regular} alt={results.alt_description} />
+                    <img src={results.urls.small} alt={results.alt_description} />
                     </div>
             ))}
           
