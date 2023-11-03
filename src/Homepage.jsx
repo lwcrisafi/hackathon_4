fc06d69f7f4f3b90de59696fc1c33d6ab11d9632
mariecotije <kkmarushka@gmail.com>
2023-11-03 14:30:12 +0100

    disabled router switch

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.scss';
-import SearchResultsSwitch from "./SearchResultsSwitch";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.scss";
+// import SearchResultsSwitch from "./SearchResultsSwitch";
 
 export default function Homepage() {
-    
-
-    return (
-      <>
-        <h1 className="homepage_headline">
-          Searchazar: The #1 Image Search Site on the Planet
-        </h1>
-        <SearchResultsSwitch/>
-      </>
-    );
-
+  return (
+    <>
+      <h1 className="homepage_headline">
+        Searchazar: The #1 Image Search Site on the Planet
+      </h1>
+      {/* <SearchResultsSwitch/> */}
+    </>
+  );
 }
--- src/SearchResultsSwitch.jsx
+++ src/SearchResultsSwitch.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react"; // Import useState
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Searchbar from "./Searchbar";
-import SearchResults from "./SearchResults";
+// import React, { useState } from "react"; // Import useState
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Searchbar from "./Searchbar";
+// import SearchResults from "./SearchResults";
 
-export default function SearchResultsSwitch() {
-  // initializing searchResults as a state variable
-  const [searchResults, setSearchResults] = useState([]);
-  const isEmptySearch = searchResults.length === 0;
+// export default function SearchResultsSwitch() {
+//   // initializing searchResults as a state variable
+//   const [searchResults, setSearchResults] = useState([]);
+//   const isEmptySearch = searchResults.length === 0;
 
-  // might need to update
-  // Update searchResults when you have search results
-  // For example, when a search is performed
-  // setSearchResults(newResults);
+//   // might need to update
+//   // Update searchResults when you have search results
+//   // For example, when a search is performed
+//   // setSearchResults(newResults);
 
-  return (
-    <Router>
-      {isEmptySearch ? (
-        <Route path="/" component={Searchbar} />
-      ) : (
-        <Route path="/" component={SearchResults} />
-      )}
-    </Router>
-  );
-}
+//   return (
+//     // <Router>
+//     //   {isEmptySearch ? (
+//     //     <Route path="/" component={Searchbar} />
+//     //   ) : (
+//     //     <Route path="/" component={SearchResults} />
+//     //   )}
+//     // </Router>
+//   );
+// }
