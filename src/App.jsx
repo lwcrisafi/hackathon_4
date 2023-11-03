11c484539b7174348b51ad4b2a8340151a2b59d4
Ephram Daniels <linguistnick7@gmail.com>
2023-11-03 13:23:13 +0100

    Lots of stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
+import Homepage from "./Homepage";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  
-
-
+  return (
+    <BrowserRouter>
+      <>
+        <Homepage />
+      </>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Homepage.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.scss';
+import SearchResultsSwitch from "./SearchResultsSwitch";
+
+export default function Homepage() {
+    
+
+    return (
+      <>
+        <h1 className="homepage_headline">
+          Searchazar: The #1 Image Search Site on the Planet
+        </h1>
+        <SearchResultsSwitch/>
+      </>
+    );
+
+}
